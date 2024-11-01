f2741b3f3ec1d3b91409733a02534acec608a50b
UwuNgu01 <48872345+uwungu01-rep@users.noreply.github.com>
2024-11-01 22:29:03 +0700

    clean up the code for a bit, less nesting, increase readability

--- caesaropt.py
+++ caesaropt.py
@@ -1,91 +1,86 @@
 import string
-alpup, alp = [*string.ascii_uppercase], [*string.ascii_lowercase]
+UPPER_ALPHABET, ALPHABET = [*string.ascii_uppercase], [*string.ascii_lowercase]
 
-def sort():
-    #Sorting algorithm, take the last characters in the alp/alpup and put it at the start of the cip/cipup for a ceratian almount of times, decided by shf
-    #The rest of the alp/alpup get put into cip/cipup without any change
-    for k in range(shf, len(alp) + shf):
-        if k < len(alp):
-            cip.append(alp[k])
-            cipup.append(alpup[k])
-            continue
-
-        cip.append(alp[abs(k - len(alp))])
-        cipup.append(alpup[abs(k - len(alp))])
-
-def encipher(alp, cip, alpup, cipup, chr):
-    oupt = ""
-    #This find the position of k in alp/alpup then add the element with the same position as k in cip/cipup to oupt
-    for k in chr:
-        if k in alp:
-            oupt += cip[alp.index(k)]
-            continue
-        elif k in alpup:
-            oupt += cipup[alpup.index(k)]
-            continue
-        oupt += k
-    return oupt
- 
-def decipher(alp, cip, cipup, alpup, chr):
-    oupt = ""
-    #This does the exact opposite as encipher
-    for k in chr:
-        if k in cip:
-            oupt += alp[cip.index(k)]
-        elif k in cipup:
-            oupt += alpup[cipup.index(k)]
+def Sort(shift) -> None:
+    #Sorting algorithm, take the last characters in the ALPHABET/UPPER_ALPHABET and put it at the start of the shifted/shifted_upper for a ceratian almount of times, decided by shift
+    #The rest of the ALPHABET/UPPER_ALPHABET get put into shifted/shifted_upper without any change
+    for k in range(shift, len(ALPHABET) + shift):
+        if k < len(ALPHABET):
+            shifted.append(ALPHABET[k])
+            shifted_upper.append(UPPER_ALPHABET[k])
         else:
-            oupt += k
-    return oupt
+            shifted.append(ALPHABET[abs(k - len(ALPHABET))])
+            shifted_upper.append(UPPER_ALPHABET[abs(k - len(ALPHABET))])
+
+def Encipher(ALPHABET, shifted, UPPER_ALPHABET, shifted_upper, user_input) -> str:
+    output = ""
+    #This find the position of k in ALPHABET/UPPER_ALPHABET then add the element with the same position as k in shifted/shifted_upper to output
+    for k in user_input:
+        if k in ALPHABET:
+            output += shifted[ALPHABET.index(k)]
+        elif k in UPPER_ALPHABET:
+            output += shifted_upper[UPPER_ALPHABET.index(k)]
+        else:
+            output += k
+    return output
+ 
+def Decipher(ALPHABET, shifted, UPPER_ALPHABET, shifted_upper, user_input) -> str:
+    output = ""
+    #This does the exact opposite as Encipher
+    for k in user_input:
+        if k in shifted:
+            output += ALPHABET[shifted.index(k)]
+        elif k in shifted_upper:
+            output += UPPER_ALPHABET[shifted_upper.index(k)]
+        else:
+            output += k
+    return output
+
+def check(shift) -> bool:
+    try:
+        int(shift)
+        return True
+    except ValueError:
+        return False
 
 while True:
-    bck = True
-    cmd = input("Type E for enciphering, type D for deciphering (case insensitive). Type / to end the program: ")
+    run = True
+    cmd = input("Type E for Enciphering, type D for Deciphering (case insensitive). Type / to end the program: ")
     if cmd == "e" or cmd == "E":
-        while True:
-            #Check if the user has input "/" to cancel enciphering or not
-            if bck:
-                chr = [*input("Your input: ")]
-                if len([x for x in chr if x != " "]) != 0:
-                    while True:
-                        cip, cipup = [], []
-                        shf = input("Shift (type / to cancel): ")
-                        if shf.isnumeric() or "-" in [x for x in shf] and [x for x in shf].index("-") == 0:
-                            shf = int(shf) % 26
-                            sort()
-                            print(f"Output: {encipher(alp, cip, alpup, cipup, chr)}")
-                        elif shf == "/":
-                            bck = False
-                            break
-                        else:
-                            print("Input has to be an integer.")
-                else:
-                    print("Input cannot be empty.")
+        while run:
+            #Check if the user has input "/" to cancel Enciphering or not
+            user_input = [*input("Your input: ").strip()]
+            if user_input:
+                while run:
+                    shifted, shifted_upper = [], []
+                    shift = input("Shift (type / to cancel): ")
+                    if check(shift):
+                        Sort(int(shift) % 26)
+                        print(f"Output: {Encipher(ALPHABET, shifted, UPPER_ALPHABET, shifted_upper, user_input)}")
+                    elif shift == "/":
+                        run = False
+                    else:
+                        print("Input has to be an integer.")
             else:
-                break
+                print("Input cannot be empty.")
                 
     #This elif block essentially do the same thing as the if block above
     elif cmd == "d" or cmd ==" D":
-        while True:
-            if bck:
-                chr = [*input("Your input: ")]
-                if len([x for x in chr if x != " "]) != 0:
-                    while True:
-                        cip, cipup = [], []
-                        shf = input("Shift (type / to cancel): ")
-                        if shf.isnumeric() or "-" in [x for x in shf] and [x for x in shf].index("-") == 0:
-                            shf = int(shf) % 26
-                            sort()
-                            print(f"Output: {decipher(alp, cip, cipup, alpup, chr)}")
-                        elif shf == "/":
-                            bck = False
-                            break
-                        else:
-                            print("Input has to be an integer.")
-                else:
-                    print("Input cannot be empty.")
+        while run:
+            user_input = [*input("Your input: ").strip()]
+            if user_input:
+                while run:
+                    shifted, shifted_upper = [], []
+                    shift = input("Shift (type / to cancel): ")
+                    if check(shift):
+                        Sort(int(shift) % 26)
+                        print(f"Output: {Decipher(ALPHABET, shifted, UPPER_ALPHABET, shifted_upper, user_input)}")
+                    elif shift == "/":
+                        run = False
+                    else:
+                        print("Input has to be an integer.")
             else:
-                break
+                print("Input cannot be empty.")
 
     elif cmd == "/":
         exit()
