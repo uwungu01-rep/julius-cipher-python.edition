88af2bff9ef65adcd3b28d229d0622ae7607c001
UwuNgu01 <48872345+uwungu01-rep@users.noreply.github.com>
2024-11-06 22:21:09 +0700

    significantly decrease the amount of lines of codes

--- caesaropt.py
+++ caesaropt.py
@@ -1,16 +1,11 @@
 import string
 UPPER_ALPHABET, ALPHABET = [*string.ascii_uppercase], [*string.ascii_lowercase]
 
-def Sort(shift) -> None:
-    #Sorting algorithm, take the last characters in the ALPHABET/UPPER_ALPHABET and put it at the start of the shifted/shifted_upper for a ceratian almount of times, decided by shift
-    #The rest of the ALPHABET/UPPER_ALPHABET get put into shifted/shifted_upper without any change
-    for k in range(shift, len(ALPHABET) + shift):
-        if k < len(ALPHABET):
-            shifted.append(ALPHABET[k])
-            shifted_upper.append(UPPER_ALPHABET[k])
-        else:
-            shifted.append(ALPHABET[abs(k - len(ALPHABET))])
-            shifted_upper.append(UPPER_ALPHABET[abs(k - len(ALPHABET))])
+def Sort(shift) -> str:
+    return ALPHABET[shift:] + ALPHABET[:shift]
+
+def Sortcap(shift) -> str:
+    return UPPER_ALPHABET[shift:] + UPPER_ALPHABET[:shift]
 
 def Encipher(ALPHABET, shifted, UPPER_ALPHABET, shifted_upper, user_input) -> str:
     output = ""
@@ -42,46 +37,29 @@ def check(shift) -> bool:
         return True
     except ValueError:
         return False
+    
+def main(param) -> None:
+    run = True
+    while run:
+        user_input = [*input("Your input: ").strip()]
+        if user_input:
+            while run:
+                shift = input("Shift (type / to cancel): ")
+                if check(shift):
+                    eval(f'print("Output:", {param}(ALPHABET, Sort(int(shift) % 26), UPPER_ALPHABET, Sortcap(int(shift) % 26), user_input))')
+                elif shift == "/":
+                    run = False
+                else:
+                    print("Input has to be an integer.")
+        else:
+            print("Input cannot be empty.")
 
 while True:
-    run = True
     cmd = input("Type E for Enciphering, type D for Deciphering (case insensitive). Type / to end the program: ")
     if cmd == "e" or cmd == "E":
-        while run:
-            #Check if the user has input "/" to cancel Enciphering or not
-            user_input = [*input("Your input: ").strip()]
-            if user_input:
-                while run:
-                    shifted, shifted_upper = [], []
-                    shift = input("Shift (type / to cancel): ")
-                    if check(shift):
-                        Sort(int(shift) % 26)
-                        print(f"Output: {Encipher(ALPHABET, shifted, UPPER_ALPHABET, shifted_upper, user_input)}")
-                    elif shift == "/":
-                        run = False
-                    else:
-                        print("Input has to be an integer.")
-            else:
-                print("Input cannot be empty.")
-                
-    #This elif block essentially do the same thing as the if block above
+        main("Encipher")
     elif cmd == "d" or cmd ==" D":
-        while run:
-            user_input = [*input("Your input: ").strip()]
-            if user_input:
-                while run:
-                    shifted, shifted_upper = [], []
-                    shift = input("Shift (type / to cancel): ")
-                    if check(shift):
-                        Sort(int(shift) % 26)
-                        print(f"Output: {Decipher(ALPHABET, shifted, UPPER_ALPHABET, shifted_upper, user_input)}")
-                    elif shift == "/":
-                        run = False
-                    else:
-                        print("Input has to be an integer.")
-            else:
-                print("Input cannot be empty.")
-
+        main("Decipher")
     elif cmd == "/":
         exit()
     else:
