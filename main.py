abeda28b8282f315a54a272a1e1ab756952853e1
UwuNgu01 <48872345+uwungu01-rep@users.noreply.github.com>
2024-12-11 14:40:43 +0700

    i did some optimization

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import string
-UPPER_ALPHABET, ALPHABET = [*string.ascii_uppercase], [*string.ascii_lowercase]
+ALPHABET, ALPHABET_UPPER = [*string.ascii_lowercase], [*string.ascii_uppercase]
+COMMAND = ["e", "E", "d", "D"]
 
-def Encipher(ALPHABET, shifted, UPPER_ALPHABET, shifted_upper, user_input) -> str:
+def Caesar(ALPHABET, shifted, UPPER_ALPHABET, shifted_upper, user_input) -> str:
     output = ""
     for k in user_input:
         if k in ALPHABET:
@@ -12,26 +13,22 @@ def Encipher(ALPHABET, shifted, UPPER_ALPHABET, shifted_upper, user_input) -> st
             output += k
     return output
 
-def Decipher(ALPHABET, shifted, UPPER_ALPHABET, shifted_upper, user_input) -> str:
-    output = ""
-    for k in user_input:
-        if k in shifted:
-            output += ALPHABET[shifted.index(k)]
-        elif k in shifted_upper:
-            output += UPPER_ALPHABET[shifted_upper.index(k)]
-        else:
-            output += k
-    return output
-
-def check(shift) -> bool:
+def IsInt(inp):
     try:
-        int(shift)
+        int(inp)
         return True
     except ValueError:
         return False
 
-def main(param) -> None:
+while True:
+    cmd = input("Type E for Enciphering, type D for Deciphering (case insensitive). Type / to end the program: ")
     run = True
+    if cmd == "/":
+        exit(0)
+    elif cmd not in COMMAND:
+        print("Invalid command.")
+        continue
+
     while run:
         user_input = [*input("Your input: ").strip()]
         if not user_input:
@@ -39,23 +36,17 @@ def main(param) -> None:
             continue
         while run:
             shift = input("Shift (type / to cancel): ")
-            if check(shift):
-                shifted =  ALPHABET[int(shift) % 26:] + ALPHABET[:int(shift) % 26]
-                shifted_upper = UPPER_ALPHABET[int(shift) % 26:] + UPPER_ALPHABET[:int(shift) % 26]
-                eval(f"print(\"Output:\", {param}(ALPHABET, shifted, UPPER_ALPHABET, shifted_upper, user_input))")
+            if IsInt(shift) and (cmd == "e" or cmd == "E"):
+                shift = int(shift)
+                shifted =  ALPHABET[shift % 26:] + ALPHABET[:shift % 26]
+                shifted_upper = ALPHABET_UPPER[shift % 26:] + ALPHABET_UPPER[:shift % 26]
+                print(f"Output: {Caesar(ALPHABET, shifted, ALPHABET_UPPER, shifted_upper, user_input)}")
+            elif IsInt(shift) and (cmd == "d" or cmd == "D"):
+                shift = -int(shift)
+                shifted =  ALPHABET[shift % 26:] + ALPHABET[:shift % 26]
+                shifted_upper = ALPHABET_UPPER[shift % 26:] + ALPHABET_UPPER[:shift % 26]
+                print(f"Output: {Caesar(ALPHABET, shifted, ALPHABET_UPPER, shifted_upper, user_input)}")
             elif shift == "/":
                 run = False
             else:
                 print("Input has to be an integer.")
-
-while True:
-    cmd = input("Type E for Enciphering, type D for Deciphering (case insensitive). Type / to end the program: ")
-    match cmd:
-        case "e" | "E":
-            main("Encipher")
-        case "d" | "D":
-            main("Decipher")
-        case "/":
-            exit()
-        case _:
-            print("Invalid command.")
