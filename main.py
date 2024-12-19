9342e6856060ff99389e3bb55fe6964f4177c760
UwuNgu01 <48872345+uwungu01-rep@users.noreply.github.com>
2024-12-19 12:52:45 +0700

    added support for processing a whole file so you won't have to copy from the terminal

--- main.py
+++ main.py
@@ -1,53 +1,145 @@
 import string
-ALPHABET, ALPHABET_UPPER = [*string.ascii_lowercase], [*string.ascii_uppercase]
-COMMAND = ["e", "E", "d", "D"]
+from os import system
+import tkinter
+import tkinter.filedialog
 
-def Caesar(ALPHABET, shifted, UPPER_ALPHABET, shifted_upper, user_input) -> str:
+root = tkinter.Tk()
+root.withdraw()
+FILETYPES = [("All files", "*.*")]
+
+ALPHABET, ALPHABET_UPPER = [*string.ascii_lowercase], [*string.ascii_uppercase]
+COMMAND = [*"12"]
+
+def check(arg1, arg2):
+    for k in arg1:
+        if k in arg2:
+            return True
+    return False
+
+def Caesar(ALPHABET, shifted, ALPHABET_UPPER, shifted_upper, user_input) -> str:
     output = ""
     for k in user_input:
         if k in ALPHABET:
             output += shifted[ALPHABET.index(k)]
-        elif k in UPPER_ALPHABET:
-            output += shifted_upper[UPPER_ALPHABET.index(k)]
+        elif k in ALPHABET_UPPER:
+            output += shifted_upper[ALPHABET_UPPER.index(k)]
         else:
             output += k
     return output
 
-def IsInt(inp):
+def fileProcessor(file_name, ALPHABET, shifted, ALPHABET_UPPER, shifted_upper) -> str:
+    with open(file_name) as data:
+        return Caesar(ALPHABET, shifted, ALPHABET_UPPER, shifted_upper, data.read())
+
+def IsInt(inp) -> bool:
     try:
         int(inp)
         return True
     except ValueError:
         return False
 
-while True:
-    cmd = input("Type E for Enciphering, type D for Deciphering (case insensitive). Type / to end the program: ")
-    if cmd == "/":
-        exit(0)
-    elif cmd not in COMMAND:
-        print("Invalid command.")
-        continue
+def main() -> None:
+    system("cls")
+    while True:
+        cmd = input("""1. Enciphering.
+2. Deciphering. 
+3. Exit. \n
+Your input: """).strip()
+        if cmd == "3":
+            exit(0)
+        elif len(cmd) == 0:
+            system("cls")
+            print("Invalid input: Empty input.")
+            continue
+        elif cmd not in COMMAND:
+            system("cls")
+            print("Invalid input: Command does not exist.")
+            continue
+        
+        system("cls")
+        mode = input("""1. Input from keyboard.
+2. Input from file.
+3. Cancel. \n
+Your input: """)
+        if mode == "3":
+            system("cls")
+            continue
 
-    while True:
-        user_input = [*input("Your input: ").strip()]
-        if not user_input:
-            print("Input cannot be empty.")
+        elif mode == "1":
+            while True:
+                user_input = [*input("Your input: ").strip()]
+                if not user_input:
+                    print("Invalid input: Empty input.")
+                else:
+                    break
+        
+            while True:
+                system("cls")
+                shift = input("Shift (type / to cancel): ").strip()
+                if IsInt(shift) and cmd == "1":
+                    shift = int(shift)
+                    shifted =  ALPHABET[shift % 26:] + ALPHABET[:shift % 26]
+                    shifted_upper = ALPHABET_UPPER[shift % 26:] + ALPHABET_UPPER[:shift % 26]
+                    print(f"Output: {Caesar(ALPHABET, shifted, ALPHABET_UPPER, shifted_upper, user_input)}")
+                elif IsInt(shift) and cmd == "2":
+                    shift = -int(shift)
+                    shifted =  ALPHABET[shift % 26:] + ALPHABET[:shift % 26]
+                    shifted_upper = ALPHABET_UPPER[shift % 26:] + ALPHABET_UPPER[:shift % 26]
+                    print(f"Output: {Caesar(ALPHABET, shifted, ALPHABET_UPPER, shifted_upper, user_input)}")
+                elif shift == "/":
+                    break
+                elif len(shift) == 0:
+                    print("Invalid input: Empty input.")
+                else:
+                    print("Invalid input: Not an integer.")
+
+        elif mode == "2":
+            system("cls")
+            while True:
+                input_file = tkinter.filedialog.askopenfilename(title="Choose your input file", filetypes=FILETYPES)
+                if len(input_file) == 0:
+                    system("cls")
+                    print("Invalid input: Please select a file.")
+                else:
+                    break
+
+            system("cls")
+            while True:
+                output_file = input("""Type the name of your output file, leave empty for the default name (output.txt)
+(If the name does not contain an extension then the program will automatically add a .txt extension): """).strip()
+                if check(output_file, [*'\\/:*?"<>|']) and len(output_file) != 0:
+                    print("Invalid input: Illegal file name (file name cannot contains \"\\/:*?\"<>|\").")
+                elif len(output_file) == 0:
+                    output_file = "output.txt"
+                    break
+                elif output_file[len(output_file) - 1] != "." or "." not in output_file:
+                    output_file += ".txt"
+                    break
+
+            while True:
+                system("cls")
+                shift = input("Shift (type / to cancel): ").strip()
+                if IsInt(shift) and cmd == "1":
+                    shift = int(shift)
+                    shifted =  ALPHABET[shift % 26:] + ALPHABET[:shift % 26]
+                    shifted_upper = ALPHABET_UPPER[shift % 26:] + ALPHABET_UPPER[:shift % 26]
+                    with open(output_file, "w") as out:
+                        out.write(fileProcessor(input_file, ALPHABET, shifted, ALPHABET_UPPER, shifted_upper))
+                elif IsInt(shift) and cmd == "2":
+                    shift = -int(shift)
+                    shifted =  ALPHABET[shift % 26:] + ALPHABET[:shift % 26]
+                    shifted_upper = ALPHABET_UPPER[shift % 26:] + ALPHABET_UPPER[:shift % 26]
+                    with open(output_file, "w") as out:
+                        out.write(fileProcessor(input_file, ALPHABET, shifted, ALPHABET_UPPER, shifted_upper))
+                elif shift == "/":
+                    system("cls")
+                    break
+                elif len(shift) == 0:
+                    print("Invalid input: Empty input.")
+                else:
+                    print("Invalid input: Not an integer.")
         else:
-            break
-    
-    while True:
-        shift = input("Shift (type / to cancel): ")
-        if IsInt(shift) and (cmd == "e" or cmd == "E"):
-            shift = int(shift)
-            shifted =  ALPHABET[shift % 26:] + ALPHABET[:shift % 26]
-            shifted_upper = ALPHABET_UPPER[shift % 26:] + ALPHABET_UPPER[:shift % 26]
-            print(f"Output: {Caesar(ALPHABET, shifted, ALPHABET_UPPER, shifted_upper, user_input)}")
-        elif IsInt(shift) and (cmd == "d" or cmd == "D"):
-            shift = -int(shift)
-            shifted =  ALPHABET[shift % 26:] + ALPHABET[:shift % 26]
-            shifted_upper = ALPHABET_UPPER[shift % 26:] + ALPHABET_UPPER[:shift % 26]
-            print(f"Output: {Caesar(ALPHABET, shifted, ALPHABET_UPPER, shifted_upper, user_input)}")
-        elif shift == "/":
-            break
-        else:
-            print("Input has to be an integer.")
+            print("Invalid input: Not a valid option.")
+        
+if __name__ == "__main__":
+    main()
